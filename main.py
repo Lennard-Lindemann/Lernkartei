50399d40fac1c177fcf21aef604fb32e8b174ea5
lennard.lindemann <Lennard.lindemann@stud.hn.de>
2021-02-26 23:59:21 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+class Questions:
+    question = ""
+
+    def set_question(self, question):
+        self.question = question
+
+
+def import_data():
+    print()
+
+
+def print_hi():
+    print("Hi")  # Press Strg+F8 to toggle the breakpoint.
+
+
+print_hi()
+import_data()
