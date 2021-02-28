1438fc7b793601a51f1d14784d9ea3e61c52dd1b
lennard.lindemann <Lennard.lindemann@stud.hn.de>
2021-02-28 20:41:50 +0100

    ausgabe_frage fertig & application ausgabe: Menü

--- main.py
+++ main.py
@@ -32,12 +32,55 @@ def ausgabe_frage(boxnr):
     tmp = Questions()
     if boxnr == 1:
         tmp = random.choice(box1)
-        print(tmp.question)
-        eingabe = input("Bitte Beantorten sie die Frag: ")
+        eingabe = input("Bitte beantworten sie die Frage: " + tmp.question + "\n")
         if eingabe == tmp.question:
             box1.remove(tmp)
             box2.append(tmp)
-            print(box2)
+            print("Frage wurde richtig beantwortet\n")
+        else:
+            print("Die Antwort is Falsch! Richtig wäre: " + tmp.answer + "\n")
+    if boxnr == 2:
+        tmp = random.choice(box2)
+        eingabe = input("Bitte beantworten sie die Frage: " + tmp.question + "\n")
+        if eingabe == tmp.question:
+            box2.remove(tmp)
+            box3.append(tmp)
+            print("Frage wurde richtig beantwortet\n")
+        else:
+            print("Die Antwort is Falsch! Richtig wäre: " + tmp.answer + "\n")
+            box2.remove(tmp)
+            box1.append(tmp)
+    if boxnr == 3:
+        tmp = random.choice(box3)
+        eingabe = input("Bitte beantworten sie die Frage: " + tmp.question + "\n")
+        if eingabe == tmp.question:
+            box3.remove(tmp)
+            box4.append(tmp)
+            print("Frage wurde richtig beantwortet\n")
+        else:
+            print("Die Antwort is Falsch! Richtig wäre: " + tmp.answer + "\n")
+            box3.remove(tmp)
+            box2.append(tmp)
+    if boxnr == 4:
+        tmp = random.choice(box4)
+        eingabe = input("Bitte beantworten sie die Frage: " + tmp.question + "\n")
+        if eingabe == tmp.question:
+            box4.remove(tmp)
+            box5.append(tmp)
+            print("Frage wurde richtig beantwortet\n")
+        else:
+            print("Die Antwort is Falsch! Richtig wäre: " + tmp.answer + "\n")
+            box4.remove(tmp)
+            box3.append(tmp)
+    if boxnr == 5:
+        tmp = random.choice(box5)
+        eingabe = input("Bitte beantworten sie die Frage: " + tmp.question + "\n")
+        if eingabe == tmp.question:
+            print("Frage wurde richtig beantwortet\n")
+        else:
+            print("Die Antwort is Falsch! Richtig wäre: " + tmp.answer + "\n")
+            box5.remove(tmp)
+            box4.append(tmp)
 
 
 def application():
@@ -83,9 +126,37 @@ def application():
     print(box1)
     print("Thema der Lernkartei:", titel)
     print(content)
-    ausgabe_frage(1)
-    # while True:
-    # print("Box 1 | Box 2 | Box 3 | Box 4 | Box 5")
+    while True:
+        print("Box (1) Fragen in der Box:", len(box1), "| Box (2)  Fragen in der Box:", len(box2)
+              , "| Box (3) Fragen in der Box:", len(box3), "| Box (4) Fragen in der Box:", len(box4)
+              , "| Box (5) Fragen in der Box:", len(box5), "| Programm schließen (0)")
+        eingabe = input()
+        if eingabe == "1":
+            if len(box1) > 0:
+                ausgabe_frage(1)
+        elif eingabe == "2":
+            if len(box2) == 0:
+                print("Die Box ist leer\n")
+                continue
+            ausgabe_frage(2)
+        elif eingabe == "3":
+            if len(box3) == 0:
+                print("Die Box ist leer\n")
+                continue
+            ausgabe_frage(3)
+        elif eingabe == "4":
+            if len(box4) == 0:
+                print("Die Box ist leer\n")
+                continue
+            ausgabe_frage(4)
+        elif eingabe == "5":
+            if len(box5) == 0:
+                print("Die Box ist leer\n")
+                continue
+            ausgabe_frage(5)
+        elif eingabe == "0":
+            print("Programm wird beendet! Danke für das Lernen")
+            break
 
 
 application()
