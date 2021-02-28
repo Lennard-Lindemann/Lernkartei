a98a025944f5b3048ee8e883e3a1a55448db6e7c
lennard.lindemann <Lennard.lindemann@stud.hn.de>
2021-02-28 20:51:27 +0100

    ausgabe_frage fertig & application ausgabe: Menü

--- main.py
+++ main.py
@@ -25,7 +25,47 @@ box5 = []
 
 
 def import_data():
-    print()
+    questionlist = []
+    titel = ""
+    komma = ','
+    f = open('Fragen&Antworten.txt', 'r')
+    content = f.read()
+    # Getting the the titel
+    titel = content.partition(komma)[0]
+    f.close()
+
+    con = content.split(',')
+    list_length = (len(con) - 1) / 2  # List_length to get the question and answer
+    i = 1
+    j = 1
+    while i <= list_length:
+        frage = Questions()
+        while j <= 2:
+            if j % 2 != 0:
+                frage.question = con[j].lstrip()
+                j += 1
+            else:
+                frage.answer = con[j]
+                j += 1
+        frage.nr = 1
+        j = 1
+        questionlist.append(frage)
+        i += 1
+
+    for i in range(len(questionlist)):
+        if questionlist[i].nr == 1:
+            box1.append(questionlist[i])
+        elif questionlist[i].nr == 2:
+            box2.append(questionlist[i])
+        elif questionlist[i].nr == 3:
+            box3.append(questionlist[i])
+        elif questionlist[i].nr == 4:
+            box4.append(questionlist[i])
+        elif questionlist[i].nr == 5:
+            box5.append(questionlist[i])
+
+    print("Thema der Lernkartei:", titel)
+    return questionlist
 
 
 def ausgabe_frage(boxnr):
@@ -84,48 +124,8 @@ def ausgabe_frage(boxnr):
 
 
 def application():
-    titel = ""
-    komma = ','
-    questionlist = []
-    f = open('Fragen&Antworten.txt', 'r')
-    content = f.read()
-    # Getting the the titel
-    titel = content.partition(komma)[0]
-    f.close()
 
-    con = content.split(',')
-    list_length = (len(con) - 1) / 2  # List_length to get the question and answer
-    i = 1
-    j = 1
-    while i <= list_length:
-        frage = Questions()
-        while j <= 2:
-            if j % 2 != 0:
-                frage.question = con[j].lstrip()
-                j += 1
-            else:
-                frage.answer = con[j]
-                j += 1
-        frage.nr = 1
-        j = 1
-        questionlist.append(frage)
-        i += 1
-
-    for i in range(len(questionlist)):
-        if questionlist[i].nr == 1:
-            box1.append(questionlist[i])
-        elif questionlist[i].nr == 2:
-            box2.append(questionlist[i])
-        elif questionlist[i].nr == 3:
-            box3.append(questionlist[i])
-        elif questionlist[i].nr == 4:
-            box4.append(questionlist[i])
-        elif questionlist[i].nr == 5:
-            box5.append(questionlist[i])
-
-    print(box1)
-    print("Thema der Lernkartei:", titel)
-    print(content)
+    import_data()
     while True:
         print("Box (1) Fragen in der Box:", len(box1), "| Box (2)  Fragen in der Box:", len(box2)
               , "| Box (3) Fragen in der Box:", len(box3), "| Box (4) Fragen in der Box:", len(box4)
